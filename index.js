4bbb5296cb0ccd472b431870d394fe856203de0d
sajib689 <jibonraja11@gmail.com>
2024-09-24 23:25:47 +0600

    add

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URI);
+        console.log("MongoDB connected successfully");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+};
+
+    app.use('/todo', todoHandler)
+
+// Error handler middleware
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) { 
+        return next(err);
+    }
+    res.status(500).json({ error: err.message });
+}
+
+// Connect to the database
+dbConnection();
+
+// Start the server
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
---
+++ routeHandler/todoHandler.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.router();
+
+
+// get the all router todo
+
+router.get('/todo', async (req, res) => {
+    
+})
