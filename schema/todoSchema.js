01fefc4e4d375e80bf173f325e899a25dc32d222
sajib689 <jibonraja11@gmail.com>
2024-09-24 23:48:23 +0600

    add todoschema

--- schema/todoSchema.js
+++ schema/todoSchema.js
@@ -4,5 +4,16 @@ const todoSchema = mongoose.Schema({
     title: {
         type: String,
         required: true,
+    },
+    description: String,
+    status: {
+        type: String,
+        enum: ['active', 'inactive']
+    },
+    date: {
+        type: Date,
+        default: Date.now()
     }
 })
+
+module.exports = todoSchema
