e1aaaf9d961548c5d81c7423b7cbd49a0bde1ed6
sajib689 <jibonraja11@gmail.com>
2024-09-25 11:01:09 +0600

    insertMany data

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -47,6 +47,13 @@ router.post('/', async (req, res) => {
 // post multiple todo
 
 router.post('/all', async (req, res) => {
+    try {
+        const todo = await Todo.insertMany(req.body)
+        res.status(200).json({message: 'inserted successfully'})
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
+   
 
 })
 
